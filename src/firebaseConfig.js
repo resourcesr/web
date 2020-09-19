63c09d5e65bfc86ba7bc8f656076fee83789cc8a
lablnet <mumerfarooqlablnet01@gmail.com>
2020-09-19 15:13:33 +0500

    hosted to firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBZGZOn9Ar-1TViCLaUL7xx7gceYvBvrNs",
-    authDomain: "zestgram-5726e.firebaseapp.com",
-    databaseURL: "https://zestgram-5726e.firebaseio.com",
-    projectId: "zestgram-5726e",
-    storageBucket: "zestgram-5726e.appspot.com",
-    messagingSenderId: "473392281375",
-    appId: "1:473392281375:web:b0f79cfe834849d436a22a",
-    measurementId: "G-GKQ0FG9D3Y"
-};
+    apiKey: "AIzaSyBILzE9CkkC1yIXIr7VSGnZpGmzRcA1FU4",
+    authDomain: "riphah-web-resources.firebaseapp.com",
+    databaseURL: "https://riphah-web-resources.firebaseio.com",
+    projectId: "riphah-web-resources",
+    storageBucket: "riphah-web-resources.appspot.com",
+    messagingSenderId: "497533596196",
+    appId: "1:497533596196:web:a138b019fde70177be1eb1",
+    measurementId: "G-F1XH2T10H8"
+  };
 
 export default firebaseConfig
