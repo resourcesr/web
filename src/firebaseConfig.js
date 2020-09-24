4430cac2db8c39d27a6198b4a4b7ed34ec06f325
lablnet <mumerfarooqlablnet01@gmail.com>
2020-09-24 07:49:34 +0500

    added firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDPtscuAqioxWMkZqpMQW-5LPubtpI6aW4",
+  authDomain: "resourcesr.firebaseapp.com",
+  databaseURL: "https://resourcesr.firebaseio.com",
+  projectId: "resourcesr",
+  storageBucket: "resourcesr.appspot.com",
+  messagingSenderId: "1097420873455",
+  appId: "1:1097420873455:web:f45cebde9928aeea81051a",
+  measurementId: "G-PWVN9EYHXS"
+};
+
+export default firebaseConfig
