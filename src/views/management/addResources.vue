b9d92ff1d5f4d671b1653db4db4b7a511eaf78d7
lablnet <mumerfarooqlablnet01@gmail.com>
2020-09-26 14:09:51 +0500

    refactor

--- src/views/management/addResources.vue
+++ src/views/management/addResources.vue
@@ -179,6 +179,7 @@ export default {
         },
     },
     mounted() {
+        this.$store.dispatch("courses/getAllCourses")
         this.$store.dispatch("resources/getResources")
     },
     computed: {
