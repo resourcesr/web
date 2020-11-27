37212dc08a0f01109bda2db589973d20daad3344
lablnet <mumerfarooqlablnet01@gmail.com>
2020-11-27 07:13:36 +0500

    added NProgress bar

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,17 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import { auth } from '../firebase'
 
+// Nprogress
+import Nprogress from 'nprogress/'
+// Nprogress CSS
+import 'nprogress/nprogress.css';
+
 Vue.use(VueRouter)
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
+        name: '',
         component: Home,
         meta: {
             breadcrumb: [
@@ -150,11 +155,23 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
 
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    Nprogress.start()
+  }
+
+  // if user is not logged in redirect to auth page.
   if (requiresAuth && !auth.currentUser) {
     next('/account')
   } else {
     next()
   }
+})
+
+router.afterEach((to, from) => {
+    // Complete the animation of the route progress bar.
+    Nprogress.done()
 })
 
 export default router
