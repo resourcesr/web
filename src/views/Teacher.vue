f750af84e9be316232c92414c8d4a0fd0a712136
lablnet <mumerfarooqlablnet01@gmail.com>
2020-11-27 07:17:45 +0500

    clean

--- src/views/Teacher.vue
+++ src/views/Teacher.vue
@@ -2,26 +2,6 @@
   <div>
         <breadcrumb name_r="Home"/>
         <dHeader /> 
-        <div class="page">
-            <v-card class="mx-auto custom-card" style="margin: 6px;" max-width="1000" >
-                <v-card-text>
-                    <v-row no-gutters class="padding: 25px">
-                        <v-col key="1" sm="10" xs="10" md="10" xl="10" lg="10" stye='height: 100px'>
-                            <div class="resource">
-                                <p class="left">
-                                    <img src="https://lahore.comsats.edu.pk/Images/employees/995-578-8587594467088994558.jpg">  
-                                </p>
-                                <div class="center">
-                                    <p class="name">Muhammad Umer Farooq</p>
-                                    <p class="cr">FC, Dean</p>
-                                    <p class="cr">FC, Dean</p>
-                                </div>
-                            </div>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-            </v-card>
-        </div>
     </div>
 </template>
 
